704db257b20bad7f9d4de0e1c46a956eb93d2ccb
m-krasikov <growield@gmail.com>
2020-02-18 23:59:10 +0200

    Start of postgres connector development

--- storage/db_connector.py
+++ storage/db_connector.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 
-class Connector(ABC):
+class DBConnector(ABC):
 
     @abstractmethod
     def connect(self):
---
+++ storage/postgres_connector.py
@@ -0,0 +1,10 @@
+import logging
+from .db_connector import DBConnector
+
+
+class PostgresConnector(DBConnector):
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
